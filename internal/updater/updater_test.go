98185213da4128c540e532b9563687a857a7d777
Stepan Vrany <stepan@vrany.dev>
2021-05-16 16:15:51 +0200

    feat: add test fo ready status

--- internal/updater/updater_test.go
+++ internal/updater/updater_test.go
@@ -10,6 +10,9 @@ import (
 //go:embed assets/idle.json
 var idleResponseString string
 
+//go:embed assets/ready.json
+var readyResponseString string
+
 func TestHandleStatesIdle(t *testing.T) {
 
 	client := &httpmock.Client{}
@@ -26,6 +29,35 @@ func TestHandleStatesIdle(t *testing.T) {
 
 	stage, stageStatus := HandleStates(client, status)
 	if stage != "noop_idle" {
+		t.Errorf("such stage title was not expected here: %s", stage)
+	}
+
+	if ! stageStatus {
+		t.Errorf("successful status was expected here")
+	}
+
+	// get status and refresh = 2
+	if client.Count > 2 {
+		t.Errorf("client was used more than twice: %d", client.Count)
+	}
+}
+
+func TestHandleStatesReady(t *testing.T) {
+
+	client := &httpmock.Client{}
+	client.SetStatusCode(200)
+	client.SetBody(readyResponseString)
+
+	status, err := bottlerocket.GetUpdatesStatus(client)
+	if err != nil {
+		t.Errorf("error was not expected here: %s", err.Error())
+	}
+
+	// update client properties a bit
+	client.SetStatusCode(204)
+
+	stage, stageStatus := HandleStates(client, status)
+	if stage != "noop_ready" {
 		t.Errorf("such stage title was not expected here: %s", stage)
 	}
 
