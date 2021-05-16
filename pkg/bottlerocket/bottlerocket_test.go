29fd6481ca00e040a5bcf65fe5974def852535ea
Stepan Vrany <stepan@vrany.dev>
2021-05-16 14:47:33 +0200

    feat: prepare unit tests

---
+++ pkg/bottlerocket/bottlerocket_test.go
@@ -0,0 +1,53 @@
+package bottlerocket
+
+import (
+	_ "embed"
+	"fmt"
+	"github.com/pipetail/bottlerocket-updater/pkg/httpmock"
+	"testing"
+)
+
+
+//go:embed assets/idle.json
+var idleResponseString string
+
+func TestActivateUpdate(t *testing.T) {
+	client := &httpmock.Client{}
+	client.SetStatusCode(204)
+
+	// pass
+	err := ActivateUpdate(client)
+	if err != nil {
+		t.Errorf("error was not expected here: %s", err.Error())
+	}
+
+	// set different status code
+	client.SetStatusCode(500)
+	err = ActivateUpdate(client)
+	if err == nil {
+		t.Errorf("an error was expected here but got nil")
+	}
+
+	// simulate the case when client returns non-nil error
+	client.SetStatusCode(500)
+	client.SetError(fmt.Errorf("random error"))
+	if err == nil {
+		t.Errorf("an error was expected here but got nil")
+	}
+}
+
+func TestGetUpdatesStatus(t *testing.T) {
+	client := &httpmock.Client{}
+	client.SetStatusCode(200)
+	client.SetBody(idleResponseString)
+
+	// test idle
+	status, err := GetUpdatesStatus(client)
+	if err != nil {
+		t.Errorf("an error was na expected here: %s", err.Error())
+	}
+
+	if status.State != "Idle" {
+		t.Errorf("Idle State was expected here but got %s", status.State)
+	}
+}
---
+++ pkg/httpmock/httpmock.go
@@ -0,0 +1,46 @@
+package httpmock
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+type Client struct{
+	Body io.ReadCloser
+	StatusCode int
+	Error error
+}
+
+func (c *Client) Get(url string) (resp *http.Response, err error) {
+	response := http.Response{
+		Body: c.Body,
+		StatusCode: c.StatusCode,
+	}
+	return &response, c.Error
+}
+
+func (c *Client) Post(url string, contentType string, body io.Reader) (resp *http.Response, err error) {
+	response := http.Response{
+		Body: c.Body,
+		StatusCode: c.StatusCode,
+	}
+	return &response, c.Error
+}
+
+func (c *Client) SetBody(body string) {
+	c.Body = ioutil.NopCloser(bytes.NewReader([]byte(body)))
+}
+
+func (c *Client) SetError(err error) {
+	c.Error = err
+}
+
+func (c *Client) ClearError() {
+	c.Error = nil
+}
+
+func (c *Client) SetStatusCode(code int) {
+	c.StatusCode = code
+}
